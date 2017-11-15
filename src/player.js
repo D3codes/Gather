85119db1fc326f74c0d2ee6f0fdb3d1913f206d0
David Freeman <davidfreeman@ksu-wireless-0-173.campus.ksu.edu>
2017-11-15 10:40:02 -0600

    added arrow controls

--- src/player.js
+++ src/player.js
@@ -27,17 +27,22 @@ export default class Player {
 
   handleKeyDown(event) {
     event.preventDefault()
+    console.log(event.key)
     switch(event.key) {
       case 'a':
+      case 'ArrowLeft':
         if(this.x > 0) this.x-=40
         break
       case 'd':
+      case 'ArrowRight':
         if(this.x < 560) this.x+=40
         break
       case 'w':
+      case 'ArrowUp':
         if(this.y > 0) this.y-=40
         break
       case 's':
+      case 'ArrowDown':
         if(this.y < 560) this.y+=40
         break
 
